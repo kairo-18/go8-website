07f60aee321dc0d640a41cbddfd4b4e7a9b7390e
kairo-18 <gladimirdungo@gmail.com>
2025-02-04 18:56:31 +0800

    added all the components together

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,35 @@ import axios from "axios";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Values from "./components/Values";
+import Project from "./components/Project";
 import Footer from "./components/Footer";
-import CoreServices from "./components/CoreServices";
 
 const App = () => {
   return (
     <>
-      
       <Navbar />
       <Hero />
       <Values />
+      <div className="w-full h-full bg-black text-white pt-10">
+        <h1 className="text-4xl text-left ml-[5%] mt-10 mb-20 font-bold text-[#2669FF]">Our Projects</h1>
+        <div>
+          <Project
+            images={["/src/assets/projects/arcus/arcus1.png", "/src/assets/projects/arcus/arcus2.png", "/src/assets/projects/arcus/arcus3.png"]}
+            title="Arcus"
+            field="Game Development"
+            description="Massively Multiplayer Online (MMO) Action Game concept, Arcus uses Bow and Arrows as weapons and blockchain technology to reward the players."
+            reversed={false}
+          />
+
+          <Project
+            images={["/src/assets/projects/aurum/aurum1.png", "/src/assets/projects/aurum/aurum2.png", "/src/assets/projects/aurum/aurum3.png"]}
+            title="Arcus"
+            field="Game Development"
+            description="Massively Multiplayer Online (MMO) Action Game concept, Arcus uses Bow and Arrows as weapons and blockchain technology to reward the players."
+            reversed={true}
+          />
+        </div>
+      </div>
       <Footer />
     </>
   );
