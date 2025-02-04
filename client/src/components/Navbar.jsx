b12227ce2ff68389fa1bf6fad393b7bb23618e58
kairo-18 <gladimirdungo@gmail.com>
2025-02-04 10:37:47 +0800

    some changes in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import logo from "/src/assets/navbar-assets/go8-logo.png";
 
 const Navbar = () => {
   return (
-    <div className='p-5 bg-black text-white'>
-        Navbar with changes
+    <div className='px-[10%] h-[100px] py-5 bg-black text-white flex justify-between'>
+        <img src={logo} alt="Logo" className='w-[70px] h-[70px]' />
+        <ul className='flex mt-5'>
+            <li className='mr-5'>Home</li>
+            <li className='mr-5'>About</li>
+            <li className='mr-5'>Contact</li>
+            <li className='mr-5'>About</li>
+            <li className='mr-5'>Contact</li>
+        </ul>
+        <a href='#' className='bg-white text-black p-2 h-10 rounded-lg'>Contact us</a>
     </div>
   )
 }
