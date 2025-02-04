1c0574d011f144f7e1ba438b0b3b51012787af8b
kairo-18 <gladimirdungo@gmail.com>
2025-02-04 11:25:30 +0800

    fully functional navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,20 +1,99 @@
-import React from 'react'
-import logo from "/src/assets/navbar-assets/go8-logo.png";
+import React from "react";
+import logo from "/src/assets/navbar-assets/go8-logo2.png";
+import barsMenu from "/src/assets/navbar-assets/bars-menu.png";
+import { useState } from "react";
 
 const Navbar = () => {
-  return (
-    <div className='px-[10%] h-[100px] py-5 bg-black text-white flex justify-between'>
-        <img src={logo} alt="Logo" className='w-[70px] h-[70px]' />
-        <ul className='flex mt-5'>
-            <li className='mr-5'>Home</li>
-            <li className='mr-5'>About</li>
-            <li className='mr-5'>Contact</li>
-            <li className='mr-5'>About</li>
-            <li className='mr-5'>Contact</li>
-        </ul>
-        <a href='#' className='bg-white text-black p-2 h-10 rounded-lg'>Contact us</a>
-    </div>
-  )
-}
+    const [showMenu, setShowMenu] = useState(false);
+    return (
+        <div className="w-full h-full flex text-white p-5 justify-between items-center bg-black">
+            <div className="logo font-bold ml-[10%] flex">
+                <img src={logo} className="w-25 h-11 mt-[-7px]" />
+            </div>
+            <ul className="hidden list-style-none gap-12 mr-[1%] lg:flex text-sm">
+                <li className="hover:transform hover:translate-0.5">
+                    <a href="#about">
+                        <span className="text-[#2669ff] font-semibold">/</span>
+                        About Us
+                    </a>
+                </li>
+                <li>
+                    <a href="#projects">
+                        <span className="text-[#2669ff] font-semibold">/</span>
+                        Projects
+                    </a>
+                </li>
+                <li>
+                    <a href="#services">
+                        <span className="text-[#2669FF] font-semibold">/</span>
+                        Core Services
+                    </a>
+                </li>
+                <li>
+                    <a href="#careers">
+                        <span className="text-[#2669FF] font-semibold">/</span>
+                        Careers
+                    </a>
+                </li>
+                <li>
+                    <a href="#news">
+                        <span className="text-[#2669FF] font-semibold">/</span>
+                        News and Articles
+                    </a>
+                </li>
+            </ul>
 
-export default Navbar
+            <a
+                href="#contact"
+                className="flex text-sm font-semibold text-black p-3 bg-white rounded-xl mr-[10%]"
+            >
+                Contact Us
+            </a>
+
+            {showMenu && (
+                <ul className="list-style-none gap-12 absolute top-[7%] right-[5%] border-1 border-gray-300 p-5">
+                    <li className=" hover:underline">
+                        <a href="#about">
+                        <span className="text-[#2669FF] font-semibold">/</span>
+                            about
+                        </a>
+                    </li>
+                    <li className=" hover:underline">
+                        <a href="#projects">
+                        <span className="text-[#2669FF] font-semibold">/</span>
+                            projects
+                        </a>
+                    </li>
+                    <li className=" hover:underline">
+                        <a href="#services">
+                        <span className="text-[#2669FF] font-semibold">/</span>
+                            services
+                        </a>
+                    </li>
+                    <li className=" hover:underline">
+                        <a href="#careers">
+                        <span className="text-[#2669FF] font-semibold">/</span>
+                            careers
+                        </a>
+                    </li>
+                    <li className=" hover:underline">
+                        <a href="#news">
+                        <span className="text-[#2669FF] font-semibold">/</span>
+                            news
+                        </a>
+                    </li>
+                </ul>
+            )}
+
+            <div id="hamburger-menu" className="lg:hidden absolute right-[5%]">
+                <img
+                    src={barsMenu}
+                    className="w-6 transform active:scale-120"
+                    onClick={() => setShowMenu(!showMenu)}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
