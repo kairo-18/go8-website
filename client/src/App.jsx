4bc8fa1e6c31aba141962e6a851f8e5617e54e10
kairo-18 <gladimirdungo@gmail.com>
2025-02-05 20:10:27 +0800

    fixed images on projects

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,12 @@ import Hero from "./components/Hero";
 import Values from "./components/Values";
 import Project from "./components/Project";
 import Footer from "./components/Footer";
+import arcus1 from '/src/assets/projects/arcus/arcus1.png';
+import arcus2 from '/src/assets/projects/arcus/arcus2.png';
+import arcus3 from '/src/assets/projects/arcus/arcus3.png';
+import aurum1 from '/src/assets/projects/aurum/aurum1.png';
+import aurum2 from '/src/assets/projects/aurum/aurum2.png';
+import aurum3 from '/src/assets/projects/aurum/aurum3.png';
 
 const App = () => {
   return (
@@ -17,7 +23,7 @@ const App = () => {
         <h1 className="text-4xl text-left ml-[5%] mt-10 mb-20 font-bold text-[#2669FF]">Our Projects</h1>
         <div>
           <Project
-            images={["/src/assets/projects/arcus/arcus1.png", "/src/assets/projects/arcus/arcus2.png", "/src/assets/projects/arcus/arcus3.png"]}
+            images = {[arcus1, arcus2, arcus3]}
             title="Arcus"
             field="Game Development"
             description="Massively Multiplayer Online (MMO) Action Game concept, Arcus uses Bow and Arrows as weapons and blockchain technology to reward the players."
@@ -25,7 +31,7 @@ const App = () => {
           />
 
           <Project
-            images={["/src/assets/projects/aurum/aurum1.png", "/src/assets/projects/aurum/aurum2.png", "/src/assets/projects/aurum/aurum3.png"]}
+            images = {[aurum1, aurum2, aurum3]}
             title="Arcus"
             field="Game Development"
             description="Massively Multiplayer Online (MMO) Action Game concept, Arcus uses Bow and Arrows as weapons and blockchain technology to reward the players."
